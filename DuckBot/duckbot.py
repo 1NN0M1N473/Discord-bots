ac2f2f98674960c3d851810fb1121a52ec648ad1
1NN0M1N473 <matt.watson2020@gmail.com>
2020-12-17 16:20:35 +0000

    Track and commit Leo's bot updates

--- DuckBot/duckbot.py
+++ DuckBot/duckbot.py
@@ -7,6 +7,7 @@
 import os
 import discord
 import random
+import typing
 from random import randint
 import discord.channel 
 import discord.client
@@ -29,6 +30,8 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 async def on_ready():
     print ("Bot Onine!")
     print ("Hello I Am " + bot.user.name)
+    print ('-----------')
+
     await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name='.help'))
 
 ###### COMMANDS #####
@@ -69,7 +72,9 @@ async def inspireme(ctx):
         
         await ctx.send('Inspirobot is broken, there is no reason to live.')
 
-### TEST ###
+##########################
+########## TEST ##########
+##########################
 
 @bot.event
 async def on_voice_state_update(member, before, after):
@@ -78,21 +83,148 @@ async def on_voice_state_update(member, before, after):
             textchannel = bot.get_channel(788226503422902343)
             await textchannel.send('Test message sent to <@349373972103561218>')
 
-#######################
-##### HELP COMMAND ####
-#######################
+########
+
+@bot.command(pass_context=True)
+async def name(ctx):
+    await ctx.send("{}".format(ctx.message.author.mention))
+
+########
+
+@bot.command()
+async def myid(ctx):
+	if ctx.message.author.id == ctx.guild.owner_id:
+		await ctx.send("{} is your id".format(ctx.message.author.id))
+	else:
+		await ctx.message.add_reaction('❌')
+
+#########
+
+@bot.command()
+async def foo(ctx, arg):
+    await ctx.send(arg)
+
+#########
+@bot.command()
+async def embedcolor(ctx):
+	embed = discord.Embed(title='TEST', description='COLOR TEST', color = ctx.me.color)
+	await ctx.send(embed=embed)
+
+########################
+##### HELP COMMAND #####
+########################
 
 bot.remove_command('help')
 
 @bot.command()
-async def help(ctx):
-    embed = discord.Embed(title='DuckBot help', description='Here is a list of available commands:', color = 0x6c3e82)
-    embed.add_field(name='.horse', value='Requested by Fel - Shows an old photo of Fel.', inline=False)
-    embed.add_field(name='.inspireme', value='Returns an AI generated image from Inspirobot.me', inline=False)
-    embed.add_field(name='.ping', value="Shwos the bot's ping to the server", inline=False)
-    embed.add_field(name='.help', value='Gives this message', inline=False)
-    embed.set_footer(text='Bot by LeoCx1000#9999', icon_url='https://i.imgur.com/DTLCaur.gif')
+async def help(ctx, argument: typing.Optional[str] = "None"):
 
-    await ctx.send(embed=embed)
+	if (argument == "None"):
+
+		# Embed title
+		embed = discord.Embed(title='DuckBot help', description=("Hey {}, Here is a list of arguments:".format(ctx.message.author.mention)), color = 0x6c3e82)
+
+		#### BLANK SPACE #####
+		embed.add_field(name='_ _', value='_ _', inline=False)
+		######################
+
+		# Commands argument
+		embed.add_field(name='.help commands', value='Show the list of normal commands', inline=False)
+
+		# Testing argument
+		embed.add_field(name='.help testing', value='shows what testing commands do. This list might not be up to date.', inline=False)
+
+		# Help command
+		embed.add_field(name='.help', value='Gives this message', inline=False)
+
+		# Made By Leo™
+		embed.set_footer(text='Bot by LeoCx1000#9999', icon_url='https://i.imgur.com/DTLCaur.gif')
+
+		#### BLANK SPACE #####
+		embed.add_field(name='_ _', value='_ _', inline=False)
+		######################
+
+		# Send embed
+		await ctx.send(embed=embed)
+
+	else:
+		if (argument == "commands"):
+
+			# Embed title
+			embed = discord.Embed(title='DuckBot help', description=("Hey {}, Here is a list of available commands:".format(ctx.message.author.mention)), color = 0x6c3e82)
+
+			#### BLANK SPACE #####
+			embed.add_field(name='_ _', value='_ _', inline=False)
+			######################
+
+			# Horse command
+			embed.add_field(name='.horse', value='Requested by Fel - Shows an old photo of Fel.', inline=False)
+
+			# InspireMe command
+			embed.add_field(name='.inspireme', value='Returns an AI generated image from Inspirobot.me', inline=False)
+
+			# Ping command
+			embed.add_field(name='.ping', value="Shwos the bot's ping to the server", inline=False)
+
+			# Help command
+			embed.add_field(name='.help', value='Gives a list of arguments', inline=False)
+
+			# Made By Leo™
+			embed.set_footer(text='Bot by LeoCx1000#9999', icon_url='https://i.imgur.com/DTLCaur.gif')
+
+			#### BLANK SPACE #####
+			embed.add_field(name='_ _', value='_ _', inline=False)
+			######################
+
+			# Send embed
+			await ctx.send(embed=embed)
+
+		else:
+			if (argument == "testing"):
+
+				# Embed title
+				embed = discord.Embed(title='DuckBot help', description=("Hey {}, Here is a list of beta/testing commands. These might not work.".format(ctx.message.author.mention)), color = 0x6c3e82)
+
+				#### BLANK SPACE #####
+				embed.add_field(name='_ _', value='_ _', inline=False)
+				######################
+
+				# MyID command
+				embed.add_field(name='.myid', value='Testing permissons for an owner-only command and adding reactions to the original command', inline=False)
+
+				# Name command
+				embed.add_field(name='.name', value='Testing on how to send a mention', inline=False)
+
+				# Foo command
+				embed.add_field(name='.foo', value="Testing on how arguments work", inline=False)
+
+				# Foo command
+				embed.add_field(name='.embedcolor', value="Testing embed color = top role color", inline=False)
+
+				# Help command
+				embed.add_field(name='.help | .help <arg>', value='Testing argument categories and optional arguments', inline=False)
+
+				# Help command
+				embed.add_field(name='.help', value='Gives a list of arguments', inline=False)
+
+				#### BLANK SPACE #####
+				embed.add_field(name='_ _', value='_ _', inline=False)
+				######################
+
+				# Made By Leo™
+				embed.set_footer(text='Bot by LeoCx1000#9999', icon_url='https://i.imgur.com/DTLCaur.gif')
+
+				# Send embed
+				await ctx.send(embed=embed)
+
+			else:
+
+				# No matching argument 
+				embed = discord.Embed(title='DuckBot help', description='Incorrect argument. type `.help` for a list of available arguments', color = 0x6c3e82)
+				await ctx.send(embed=embed)
+
+###################
+####### RUN #######
+###################
 
 bot.run(TOKEN)
