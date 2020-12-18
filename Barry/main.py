9fbb4437e304ed98033eb5d475c56a90eaee4f20
1NN0M1N473 <matt.watson2020@gmail.com>
2020-12-18 10:56:08 -0600

    Add help command for Barry

--- Barry/main.py
+++ Barry/main.py
@@ -96,6 +96,57 @@ async def barry(ctx, *, input):
     await ctx.send(response)
 
 
+### HELP COMMAND ###
+
+bot.remove_command('help')
+
+@bot.command()
+async def help(ctx, argument: typing.Optional[str] = "None"):
+
+    if (argument = "None"):
+
+        embed = discord.Embed(title='Barry help', description=("Hi there {}! Here are the commands you can use with this bot:".format(ctx.message.author.mention)), color = 0x00FFFF)
+
+        embed.add_field(name='_ _', value='_ _', inline=False)
+
+        embed.add_field(name='?help commands', value='Shows the commands you can run. Currently the only argument, more to come!', inline=False)
+
+        embed.add_field(name='?help', value='Displays this help screen', inline=False)
+
+        embed.set_footer(text='This bot was created by 1NN0M1N473#1337', icon_url='https://i.imgur.com/SRvpDj5.jpg')
+
+        embed.add_field(name='_ _', value='_ _', inline=False)
+
+        await ctx.send(embed=embed)
+
+    else:
+        if (argument == "commands"):
+
+            embed = discord.Embed(title='Barry help', description=("Hi there {}! Here are the commands you can use with this bot:".format(ctx.message.author.mention)), color = 0x00FFFF)
+
+            embed.add_field(name='_ _', value='_ _', inline=False)
+
+            embed.add_field(name='?ping', value='Measures your latency!', inline=False)
+
+            embed.add_field(name='?help', value='Displays usable commands', inline=False)
+
+            embed.add_field(name='?quote', value='Fetches a random quote from the ZenQuote API', inline=False)
+
+            embed.add_field(name='?nerdyquote', value="Fetches a random programming-themed quote", inline=False)
+
+            embed.add_field(name='?beeattack', value="Summons a horde of angry bees, use with caution", inline=False)
+
+            embed.add_field(name='?barry <say something>', value="Engage in conversation with the bot; responses not guaranteed to make sense", inline=False)
+
+            embed.add_field(name='_ _', value='_ _', inline=False)
+
+            await ctx.send(embed=embed)
+
+    else:
+
+        embed = discord.Embed(title='Barry help', description='No command found. What exactly are you trying to accomplish? Run ?help again.', color = 0xFF0000)
+        await ctx.send(embed=embed)
+
 
 ### RUN BOT ###
 
